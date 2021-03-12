ce9d17588dcbb52af6e7b32f8c1351907f9063c6
mazeau <mazeau.e@husky.neu.edu>
2021-03-12 14:21:42 -0500

    WIP: add coverage dep to __repr__ from string

--- rmgpy/kinetics/surface.pyx
+++ rmgpy/kinetics/surface.pyx
@@ -81,7 +81,6 @@ cdef class StickingCoefficient(KineticsModel):
         StickingCoefficient object.
         """
         string = 'StickingCoefficient(A={0!r}, n={1!r}, Ea={2!r}, T0={3!r}'.format(self.A, self.n, self.Ea, self.T0)
-        # if self.cov is not None: string += ", {}, {'E':({0!r}, {}), 'm':{}, 'a':{}}"
         if self.Tmin is not None: string += ', Tmin={0!r}'.format(self.Tmin)
         if self.Tmax is not None: string += ', Tmax={0!r}'.format(self.Tmax)
         if self.cov is not None:
