2895c1c743ee3d363616f6a0dff8b17d0e826d53
Kevin Spiekermann <kspieker@mit.edu>
2021-05-22 12:18:32 -0400

    OrcaLog: add method that checks for common errors

--- arkane/ess/orca.py
+++ arkane/ess/orca.py
@@ -32,8 +32,8 @@ Arkane Orca module
 Used to parse Orca output files
 """
 
-import logging
 import numpy
+import os.path
 import rmgpy.constants as constants
 
 from arkane.common import get_element_mass
@@ -52,8 +52,35 @@ class OrcaLog(ESSAdapter):
     arrays. OrcaLog is an adapter for the abstract class ESSAdapter.
     """
 
-    def __init__(self, path):
+    def __init__(self, path, check_for_errors=True):
         self.path = path
+        if check_for_errors:
+            self.check_for_errors()
+
+    def check_for_errors(self):
+        """
+        Checks for common errors in an Orca log file.
+        If any are found, this method will raise an error and crash.
+        """
+        with open(os.path.join(self.path), 'r') as f:
+            lines = f.readlines()
+            error = None
+            for line in reversed(lines):
+                # check for common error messages
+                if 'ORCA finished by error termination in SCF' in line:
+                    error = 'SCF'
+                    break
+                elif 'ORCA finished by error termination in MDCI' in line:
+                    error = 'MDCI'
+                    break
+                elif 'Error : multiplicity' in line:
+                    error = f'The multiplicity and charge combination for species {species_label} are wrong.'
+                    break
+                elif 'ORCA TERMINATED NORMALLY' in line:
+                    break
+
+            if error:
+                raise LogError(f'There was an error with Orca output file {self.path} due to line:\n{line}')
 
     def get_number_of_atoms(self):
         """
@@ -94,19 +121,6 @@ class OrcaLog(ESSAdapter):
 
         with open(self.path) as f:
             log = f.readlines()
-
-        # First check that the Orca job file (not necessarily a geometry optimization)
-        # has successfully completed, if not an error is thrown
-        completed_job = False
-        for line in reversed(log):
-            if 'ORCA TERMINATED NORMALLY' in line:
-                logging.debug('Found a successfully completed Orca Job')
-                completed_job = True
-                break
-
-        if not completed_job:
-            raise LogError(
-                'Could not find a successfully completed Orca job in Orca output file {0}'.format(self.path))
 
         # Now look for the geometry.
         # Will return the final geometry in the file under Standard Nuclear Orientation.
